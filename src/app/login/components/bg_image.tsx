61f080703727f47a1687f69b575913c1083a7269
Pedro Ferreira Galvão <pedrogalvao@discente.ufg.br>
2023-05-28 01:36:10 -0300

    Page Login - estilizada e responsiva

---
+++ src/app/login/components/bg_image.tsx
@@ -0,0 +1,13 @@
+export const LoginImage = () => {
+  return (
+  <div className="relative hidden flex-1 lg:block">
+    <img
+      className="h-full object-cover"
+      src="https://images.unsplash.com/photo-1599687351724-dfa3c4ff81b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+      alt="imagem login/cadastro"
+    />
+  </div>
+  )
+}
+//https://images.unsplash.com/photo-1599687351724-dfa3c4ff81b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80
+//https://images.unsplash.com/photo-1516321497487-e288fb19713f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,15 @@
+import { SectionContent } from '@/components/interface/section-content'
 import { Heading } from '@/components/ui/heading'
+import { LoginImage } from './components/bg_image'
+import { LoginForms } from './components/forms'
 
 const Login = () => {
   return (
-    <div>
-      <main>
-        <Heading>Login teste</Heading>
-      </main>
-    </div>
+      <div className="flex min-h-full pt-5 mx-auto my-auto max-w-7xl sm:pl-8">
+        <LoginForms/>
+        <LoginImage/>
+
+      </div>
   )
 }
 
